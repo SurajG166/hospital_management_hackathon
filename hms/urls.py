2bad723a4be3dfdf45f24a774d40acf10dae0607
skia <sairagav2003@gmail.com>
2024-07-23 01:04:37 +0530

    FIXED LOGIN <3 GNgit add .

--- hms/urls.py
+++ hms/urls.py
@@ -6,9 +6,9 @@ urlpatterns = [
     path("home/", views.serve_home_page),
     path("login/", views.serve_login_page, name="login_page"),
     path("validate_login/", views.validate_login, name="validate_login"),
-    path("patient_home/", views.patient_home, name="patient_home"),
-    path("doctor_home/", views.doctor_home, name="doctor_home"),
-    path("hospital_home/", views.hospital_home, name="hospital_home"),
+    path("patient_home/<int:patient_id>/", views.patient_home, name="patient_home"),
+    path("doctor_home/<int:doctor_id>/", views.doctor_home, name="doctor_home"),
+    path("hospital_home/<int:hospital_id>/", views.hospital_home, name="hospital_home"),
     path(
         "prescription_form/",
         views.serve_prescription_form,

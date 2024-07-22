64303744fa48259e568bb199c1ae6eb56e183e19
Uday M <uday2003m@gmail.com>
2024-07-22 16:00:41 +0530

    models manytomany

--- hms/models.py
+++ hms/models.py
@@ -1,30 +1,48 @@
 from django.db import models
 
-# Create your models here.
 class Hospital(models.Model):
-    id = models.IntegerField(unique=True)
     name = models.CharField(max_length=50)
-    loc=models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
 
-    # def _str_(self):
-    #     return self.name
+    def __str__(self):
+        return self.name
 
 class Doctor(models.Model):
-    id=models.IntegerField(unique=True)
     name = models.CharField(max_length=200)
-    age=models.IntegerField()
+    age = models.IntegerField()
     qualification = models.CharField(max_length=10)
-    contact=models.IntegerField()
-    
+    contact = models.CharField(max_length=15)
+    hospitals = models.ManyToManyField(Hospital, through='Schedule')
 
-    # def _str_(self):
-    #     return self.title
-    
-class patient(models.Model):
-    id=models.IntegerField(unique=True)
+    def __str__(self):
+        return self.name
+
+class Patient(models.Model):
     name = models.CharField(max_length=200)
-    age=models.IntegerField()
-    condition= models.CharField(max_length=10)
-    contact=models.IntegerField()
-    address=models.CharField(max_length=200)
-    
+    age = models.IntegerField()
+    condition = models.CharField(max_length=10)
+    contact = models.CharField(max_length=15)
+    address = models.CharField(max_length=200)
+    hospital = models.ForeignKey(Hospital, on_delete=models.CASCADE)
+    doctors = models.ManyToManyField(Doctor, through='Report')
+
+    def __str__(self):
+        return self.name
+
+class Schedule(models.Model):
+    hospital = models.ForeignKey(Hospital, on_delete=models.CASCADE)
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    schedule = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.doctor.name} at {self.hospital.name} - {self.schedule}"
+
+class Report(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    prescription = models.TextField()
+    date = models.DateField()
+    diagnosis = models.TextField()
+
+    def __str__(self):
+        return f"Report for {self.patient.name} by {self.doctor.name} on {self.date}"
